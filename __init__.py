2fdcad798b68a31c289acdc4871d82cf111f44a1
SerkanHub <ozerserkan05@gmail.com>
2026-01-28 16:09:14 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -186,4 +186,4 @@ def api_search_livres():
 
                                                                                                                                    
 if __name__ == "__main__":
-app.run(debug=True)
+    app.run(debug=True)
