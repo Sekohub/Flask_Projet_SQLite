3287b85b2e775b41bc2f2cdd33176c7f1f682c5e
SerkanHub <ozerserkan05@gmail.com>
2026-02-18 15:34:24 +0100

    Update schema.sql

--- task_app/schema.sql
+++ task_app/schema.sql
@@ -1,1 +1,8 @@
+CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titre TEXT NOT NULL,
+    description TEXT NOT NULL,
+    date_echeance TEXT NOT NULL,
+    terminee INTEGER DEFAULT 0
+);
 
