bf46046af664853d898e35936b39cc6ef2ae9228
SerkanHub <ozerserkan05@gmail.com>
2026-02-18 15:35:46 +0100

    Update create_db.py

--- task_app/create_db.py
+++ task_app/create_db.py
@@ -1,1 +1,17 @@
+import sqlite3
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+DB_PATH = os.path.join(BASE_DIR, "tasks.db")
+SCHEMA_PATH = os.path.join(BASE_DIR, "schema.sql")
+
+conn = sqlite3.connect(DB_PATH)
+
+with open(SCHEMA_PATH) as f:
+    conn.executescript(f.read())
+
+conn.commit()
+conn.close()
+
+print("\o/ Base tasks.db créée avec succès !")
 
